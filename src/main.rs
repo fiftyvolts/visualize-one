a55225864c7286934887f334f87cb46eaec11887
fiftyvolts <46228821+fiftyvolts@users.noreply.github.com>
2022-12-31 22:10:38 -0500

    added initial rendering library

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,45 @@
+extern crate gl;
 extern crate sdl2;
 
+use std;
 
 fn main() {
-    let _sdl = sdl2::init().unwrap();
+    let sdl = sdl2::init().unwrap();
+
+    let video_subsystem = sdl.video().unwrap();
+    let gl_attr = video_subsystem.gl_attr();
+
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 5);
+
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let mut event_pump = sdl.event_pump().unwrap();
+
+    unsafe {
+        gl::Viewport(0, 0, 900, 700); // set viewport
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
+            }
+        }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
+    }
 }
