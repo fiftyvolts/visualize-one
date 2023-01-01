6f563756554e9138cea3a8247fa6666bb27d5215
fiftyvolts <46228821+fiftyvolts@users.noreply.github.com>
2023-01-01 08:20:42 -0500

    triangle with rainbow colors

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,42 @@ extern crate gl;
 extern crate sdl2;
 
 use std;
+use std::ffi::CString;
+
+pub mod render_gl;
+
+#[derive(Debug,Clone,Copy)]
+struct Vertex {
+    data: [f32;6]
+}
+
+impl Vertex {
+    fn new(x: f32, y: f32, z: f32, r: f32, g: f32, b: f32) -> Self {
+        Vertex { data: [x, y, z, r, g, b] }
+    }
+
+    fn set_pos(&mut self, x: f32, y: f32, z: f32) {
+        self.data[0] = x;
+        self.data[1] = y;
+        self.data[2] = z;
+    }
+
+    fn set_color(&mut self, r: f32, g: f32, b: f32) {
+        self.data[3] = r;
+        self.data[4] = g;
+        self.data[5] = b;
+    }
+}
+
+impl FromIterator<Vertex> for Vec<f32> {
+    fn from_iter<T: IntoIterator<Item = Vertex>>(iter: T) -> Self {
+        let mut v = vec![];
+        for item in iter {
+            v.extend_from_slice(&item.data);
+        }
+        v
+    }
+}
 
 fn main() {
     let sdl = sdl2::init().unwrap();
@@ -10,7 +46,7 @@ fn main() {
     let gl_attr = video_subsystem.gl_attr();
 
     gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-    gl_attr.set_context_version(4, 5);
+    gl_attr.set_context_version(3, 3);
 
     let window = video_subsystem
         .window("Game", 900, 700)
@@ -18,6 +54,7 @@ fn main() {
         .resizable()
         .build()
         .unwrap();
+
     let _gl_context = window.gl_create_context().unwrap();
     let _gl =
         gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
@@ -27,6 +64,69 @@ fn main() {
         gl::Viewport(0, 0, 900, 700); // set viewport
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
+
+    let vert_shader =
+        render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap())
+            .unwrap();
+
+    let frag_shader =
+        render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap())
+            .unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+
+    let vertices = [
+        Vertex::new(-0.5, -0.5, 0.0, 1.0, 0.0, 1.0),
+        Vertex::new(0.5, -0.5, 0.0, 0.0, 1.0, 0.0),
+        Vertex::new(0.0, 0.5, 0.0, 0.0, 0.0, 1.0),
+    ];
+
+    let vbuff: Vec<f32> = vertices.iter().cloned().collect();
+
+    let mut vbo: gl::types::GLuint = 0;
+
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,                                                       // target
+            (vbuff.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
+            vbuff.as_ptr() as *const gl::types::GLvoid, // pointer to data
+            gl::STATIC_DRAW,                               // usage
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0); // unbind the buffer
+    }
+
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+
+        gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
+        gl::VertexAttribPointer(
+            0,         // index of the generic vertex attribute ("layout (location = 0)")
+            3,         // the number of components per generic vertex attribute
+            gl::FLOAT, // data type
+            gl::FALSE, // normalized (int-to-float conversion)
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+            std::ptr::null(),                                     // offset of the first component
+        );
+
+        gl::EnableVertexAttribArray(1); // this is "layout (location = 0)" in vertex shader
+     gl::VertexAttribPointer(
+        1, // index of the generic vertex attribute ("layout (location = 0)")
+        3, // the number of components per generic vertex attribute
+        gl::FLOAT, // data type
+        gl::FALSE, // normalized (int-to-float conversion)
+        (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+        (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid // offset of the first component
+    );
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
 
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -38,6 +138,16 @@ fn main() {
 
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        shader_program.set_used();
+        unsafe {
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(
+                gl::TRIANGLES, // mode
+                0,             // starting index in the enabled arrays
+                3,             // number of indices to be rendered
+            );
         }
 
         window.gl_swap_window();
--- src/render_gl.rs
+++ src/render_gl.rs
@@ -1,9 +1,7 @@
-pub mod render_gl;
-
 use gl;
 use std;
 use std::ffi::{CStr, CString};
-use std::repeat;
+use std::iter::repeat;
 
 pub struct Shader {
     id: gl::types::GLuint,
@@ -70,4 +68,78 @@ impl Drop for Shader {
 
 fn create_whitespace_cstring_with_len(len: usize) -> CString {
     unsafe { CString::from_vec_unchecked(repeat(b' ').take(len).collect()) }
+}
+
+pub struct Program {
+    id: gl::types::GLuint,
+}
+
+impl Program {
+    pub fn from_shaders(shaders: &[Shader]) -> Result<Program, String> {
+        let program_id = unsafe { gl::CreateProgram() };
+
+        for shader in shaders {
+            unsafe {
+                gl::AttachShader(program_id, shader.id());
+            }
+        }
+
+        unsafe {
+            gl::LinkProgram(program_id);
+        }
+
+        let mut success: gl::types::GLint = 1;
+
+        unsafe {
+            gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut success);
+        }
+
+        if success == 0 {
+            let mut len: gl::types::GLint = 0;
+            unsafe {
+                gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut len);
+            }
+
+            let error = create_whitespace_cstring_with_len(len as usize);
+
+            unsafe {
+                gl::GetProgramInfoLog(
+                    program_id,
+                    len,
+                    std::ptr::null_mut(),
+                    error.as_ptr() as *mut gl::types::GLchar,
+                );
+            }
+
+            return Err(error.to_string_lossy().into_owned());
+        }
+
+        // continue with error handling here
+
+        for shader in shaders {
+            unsafe {
+                gl::DetachShader(program_id, shader.id());
+            }
+        }
+
+        Ok(Program { id: program_id })
+    }
+
+    pub fn id(&self) -> gl::types::GLuint {
+        self.id
+    }
+
+    pub fn set_used(&self) {
+        unsafe {
+            gl::UseProgram(self.id());
+        }
+    }
+}
+
+impl Drop for Program {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteProgram(self.id);
+        }
+    }
 }
